4c43af0f532424f496fc3c226358db8308e0282b
Katya <accounteight@proton.me>
2024-12-30 00:46:49 +0300

    version 03. In third game version was added total execution time

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
-
+import time
 def func():
     symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{};:'|,.<>/?"
     password = input("Enter password: ")
+    time_start = time.time()
     length_password = len(password)
     counts = 0
     enigma = ''.join(random.choices(symbols, k=length_password))
@@ -10,6 +11,7 @@ def func():
         enigma = ''.join(random.choices(symbols, k=length_password))
         counts = counts + 1
         print(enigma, counts)
-    print(f'Your password: {password}\nEnigma: {enigma} Total selected: {counts}')
+    end_time = time.time()
+    print(f'Your password: {password}\nEnigma: {enigma} \nTotal selected: {counts}\nTime: {round(end_time-time_start, ndigits=2)} sec')
 func()
 
