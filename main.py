e5e585bc336744741b8c26338f38816180792752
Katya <accounteight@proton.me>
2024-12-30 00:31:53 +0300

    version 02 Second version of game with output Your password, Enigma password and total selected passwords combination

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import random
+
 def func():
+    symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{};:'|,.<>/?"
     password = input("Enter password: ")
-    return password
-result = func()
+    length_password = len(password)
+    counts = 0
+    enigma = ''.join(random.choices(symbols, k=length_password))
+    while enigma != password:
+        enigma = ''.join(random.choices(symbols, k=length_password))
+        counts = counts + 1
+        print(enigma, counts)
+    print(f'Your password: {password}\nEnigma: {enigma} Total selected: {counts}')
+func()
+
